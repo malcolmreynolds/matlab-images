16c102bdbb9524266e05b51a23228508ea23cd31
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2009-12-01 13:46:02 +0000

    Corrected comment in blur_linear_cart.

--- blur_linear_cart.m
+++ blur_linear_cart.m
@@ -1,4 +1,6 @@
 function br = blur_linear_cart(Im,xb,yb)
-% Blurs image I in the direction given by (xb,yb).
+% Blurs image Im in the direction given by (xb,yb).
 k = fspecial('motion',sqrt(xb^2 + yb^2),(atan(yb/xb)/pi)*180);
 br = imfilter(Im,k,'conv','replicate');
+
+
