01cb4144eaf76c809e144b7c170d2241c0ed37ca
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2009-12-16 18:14:56 +0000

    Rewrote alpha_hist to cover rgb and Lab.
    Now it's time to really rock the funky beat.

--- alpha_hist.m
+++ alpha_hist.m
@@ -1,7 +1,8 @@
-function bins = alpha_hist(Im,A)
+function hist = alpha_hist(Im,A,transform)
 %Histogram of RGB values for an RGB image w/ alpha mask
 %Takes an alpha mask which CAN be
-%fractional. Im must be uint8!
+%fractional. Im must be uint8! Transform, if present, gives some
+%transform that should be done with colorspace.m before doing the conversion.
 
 if (size(Im,3) ~= 3),
   error('alpha_hist:numberOfSamples', 'Input image must be RGB');
@@ -13,30 +14,63 @@ elseif max(max(A)) > 1,
   warning('alpha_hist:amaskValuesTooLarge','alpha mask contains values greater than 1!');
 end
 
+if isempty(transform),
+  transform = 'rgb'; % default, means we don't do a transform
+end
+
 numpixels = prod(size(A));
 
 %get our reshaped images and mask, setup bins
 A_vec = reshape(A,1,numpixels);
-bins.r = zeros(1,256);
-bins.g = zeros(1,256);
-bins.b = zeros(1,256);
+ 
+%here we setup each line. each struct contains a label identifying
+%what the line signifies, then the x range, then the y range (ie
+%the bins.
+switch transform
+ case 'rgb'
+  hist.lines{1} = {'r', 0:255, zeros(1,256),0};
+  hist.lines{2} = {'g', 0:255, zeros(1,256),0};
+  hist.lines{3} = {'b', 0:255, zeros(1,256),0};
+  hist.meta = 'rgb histogram'; 
+ case 'RGB->lab'
+  Im = colorspace(transform,Im);
+  hist.lines{1} = {'l',  0:100,zeros(1,101),1};
+  hist.lines{2} = {'a^*',-100:1:100,zeros(1,201),101};
+  hist.lines{3} = {'b^*',-100:1:100,zeros(1,201),101};
+  hist.meta = 'CIE 1976 (L^*, a^*, b^*) histogram';
+ otherwise
+  error('unsupported transform %s',transform);
+end
 
-%red
-Im_vec_r = double(reshape(Im(:,:,1),1,numpixels));
-Im_vec_g = double(reshape(Im(:,:,2),1,numpixels));
-Im_vec_b = double(reshape(Im(:,:,3),1,numpixels));
+%put each channel into a long vector, convert to doubles and round
+im_chan_1 = round(double(reshape(Im(:,:,1),1,numpixels)));
+im_chan_2 = round(double(reshape(Im(:,:,2),1,numpixels)));
+im_chan_3 = round(double(reshape(Im(:,:,3),1,numpixels)));
+hl1 = hist.lines{1};
+hl2 = hist.lines{2};
+hl3 = hist.lines{3};
+
+%for the colour ranges which go negative, we need to cope with this
+offsets = [hl1{4}, hl2{4}, hl3{4}];
 for p=1:numpixels,
+  
   amt = A_vec(p);
-  bins.r(Im_vec_r(p)+1) = bins.r(Im_vec_r(p)+1) + amt;
-  bins.g(Im_vec_g(p)+1) = bins.g(Im_vec_g(p)+1) + amt;
-  bins.b(Im_vec_b(p)+1) = bins.b(Im_vec_b(p)+1) + amt;
+  
+  %yeah, who needs the += operator when we have this kind of
+  %fearful conciseness going on.
+  hl1{3}(im_chan_1(p)+offsets(1)) = ...
+      hl1{3}(im_chan_1(p)+offsets(1)) + amt;
+  hl2{3}(im_chan_2(p)+offsets(2)) = ...
+      hl2{3}(im_chan_2(p)+offsets(2)) + amt;
+  hl3{3}(im_chan_3(p)+offsets(3)) = ...
+      hl3{3}(im_chan_3(p)+offsets(3)) + amt;
 end
 
 total_alpha = sum(A_vec);
 
 %normalise by number of pixels
-bins.r = bins.r ./ total_alpha;
-bins.g = bins.g ./ total_alpha;
-bins.b = bins.b ./ total_alpha;
+hl1{3} = hl1{3} ./ total_alpha;
+hl2{3} = hl2{3} ./ total_alpha;
+hl3{3} = hl3{3} ./ total_alpha;
 
 
