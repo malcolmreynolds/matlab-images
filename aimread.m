f52e24733eef241c8bddd6ed4917d642e3fab9f0
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2010-01-05 12:30:28 +0000

    Hack 'fix' for transparent images that load white in see-through part.

--- aimread.m
+++ aimread.m
@@ -1,4 +1,9 @@
 function [im map mask] = aimread(filename);
-% Wrapper to avoid having to normalise the alpha mask
+% Wrapper to avoid having to normalise the alpha mask and also fix the
+% ridiculous problem that sometimes at randomly determined points, i end up
+% with a png that will always be loaded with white in its transparent bits
+% instead of black. That doesn't work out that well.
 [im map mask] = imread(filename);
+%maybe one day this bullshit won't be necessary
+im(im == 255) = 0;
 mask = double(mask) ./ 255;
--- composite.m
+++ composite.m
@@ -1,4 +1,4 @@
-function bg = composite(bg,fg,amask,offsetwidth,offsetheight)
+function bg = composite(bg,fg,amask,offsetx,offsety)
 % Composite the fg image with associate alpha mask onto bg image.
 % Note that the parts of fg which are supposed to be transparent MUST
 % be black or else everything in this won't work... offset must
@@ -12,8 +12,10 @@ if size(amask,3) == 1,
 end
 
 if nargin == 3,
-  offset.width = 0;
-  offset.height = 0;
+  offsetx = 0;
+  offsety = 0;
+elseif nargin ~= 5,
+  error('composite:WrongNumberOfArgs','Need 3 or 5 arguments');
 end
 % this is ad-hoc as fuck. maybe a bad idea. but for now, let's say
 % if we have any values over 1 then assume this is a 0-255 image so
@@ -37,8 +39,8 @@ end
 % the outside is black then it should already have that taken into
 % account. Multiplying by alpha would just make it even smaller
 % again. - APPROVED by Gabe.
-regionx = (offsetwidth+1):(offsetwidth + size(fg,2));
-regiony = (offsetheight+1):(offsetheight + size(fg,1)); 
+regionx = (offsetx+1):(offsetx + size(fg,2));
+regiony = (offsety+1):(offsety + size(fg,1)); 
 bg_for_composite = double(bg(regiony,regionx,:));
 bg(regiony,regionx,:) = uint8((1-amask).*bg_for_composite + double(fg));
 
